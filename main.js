0745e5ba976f86bff7d0763be99184ee4ea9e48a
quanghuy24121999 <quanghuy24121999@gmail.com>
2022-05-03 20:59:46 +0700

    update home page

---
+++ main.js
@@ -0,0 +1,44 @@
+// $("#mnt .dm").click(function() {
+//     $(this).parent().parent().parent().find(".sub-menu").toggle(400);
+// });
+
+let e1 = document.getElementById('dm1');
+let e2 = document.getElementById('dm2');
+let flag1 = false;
+let flag2 = false;
+
+if (e1) {
+    e1.onclick = () => {
+        if (document.getElementById('sub-menu-1') && flag1 == false) {
+            document.getElementById('sub-menu-1').style.display = "block";
+            flag1 = true;
+            flag2 = false;
+        } else if (document.getElementById('sub-menu-1') && flag1 == true) {
+            document.getElementById('sub-menu-1').style.display = "none";
+            flag1 = false;
+            flag2 = false;
+        }
+
+        if (document.getElementById('sub-menu-2')) {
+            document.getElementById('sub-menu-2').style.display = "none";
+        }
+    }
+}
+
+if (e2) {
+    e2.onclick = () => {
+        if (document.getElementById('sub-menu-2') && flag2 == false) {
+            document.getElementById('sub-menu-2').style.display = "block";
+            flag2 = true;
+            flag1 = false;
+        } else if (document.getElementById('sub-menu-2') && flag2 == true) {
+            document.getElementById('sub-menu-2').style.display = "none";
+            flag2 = false;
+            flag1 = false;
+        }
+
+        if (document.getElementById('sub-menu-1')) {
+            document.getElementById('sub-menu-1').style.display = "none";
+        }
+    }
+}
